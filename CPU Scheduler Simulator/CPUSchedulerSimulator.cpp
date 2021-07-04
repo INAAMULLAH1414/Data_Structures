37d18bc17c3fcd15a6a4d60b14d3372eae70cff6
INAAMULLAH1414 <56350545+INAAMULLAH1414@users.noreply.github.com>
2021-07-04 10:33:38 +0500

    CPU, CL Added

---
+++ CPU Scheduler Simulator/CPUSchedulerSimulator.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <conio.h>
+#include <queue>
+using namespace std;
+
+struct Process
+{
+	string name;	//Process Name
+	int exeTime;	//Execution Time
+	int arrTime;	//Arrival Time
+};
+
+int sizeOfLines(ifstream &in);
+void readProcesses(ifstream &in, queue<Process> &Q, const int arrTime);
+ifstream &operator>> (ifstream &in, Process &p);
+ostream &operator<< (ostream &cout, const Process &p);
+void displayQueue(queue<Process> &m);
+int main()
+{
+	ifstream in;int waitTime = 0;int turnTime = 0;int totalTime = 0;
+	float avgWaitTime = 0;float avgTurnTime = 0;int count = 0;
+	queue<Process> Q;
+	in.open("readylist.txt");
+	if (!in || in.peek() == -1)	{cout << "No processes found!" << endl;         exit(0);}
+	readProcesses(in, Q, 0);
+	while (in.peek() != -1 || !Q.empty())
+	{count++;
+   	 waitTime = totalTime - Q.front().arrTime;
+		for (int i = 1; i <= Q.front().exeTime; i++)
+		{totalTime++;
+		    if (totalTime % 20== 0)
+			{cout << "\nPress <Enter> to continue...\n" << endl;
+			      
+				while (1)
+				{char c = _getch();
+				  if (c == '\r'){   displayQueue(Q); readProcesses(in, Q, totalTime);  		break;}
+				}
+			}
+		}
+		turnTime = totalTime - Q.front().arrTime;
+		avgWaitTime += waitTime;		avgTurnTime += turnTime;
+        cout<<endl<<"Program Executed:\t";
+		cout << Q.front() << ", " << waitTime << ", " << turnTime << endl;
+		Q.pop();
+	}
+	avgWaitTime /= count;
+	avgTurnTime /= count;
+	cout<<endl<<"Now,No process in the ready queue.Queue is empty.....!"<<endl;
+	cout << "\n\nAverage Waiting Time: " << avgWaitTime << endl;
+	cout << "Average Turnaround Time: " << avgTurnTime << endl;
+	in.close();
+}
+ifstream &operator>> (ifstream &in, Process &p){getline(in, p.name,' '); in >> p.exeTime;	in.ignore(); return in;}
+
+ostream &operator<< (ostream &out, const Process &p){	out << p.name << ", " << p.exeTime;	return out; }
+
+int sizeOfLines(ifstream &in){
+	
+	int size = 0;     in.seekg(0);
+	while (!in.eof() && in.peek() != -1){in.ignore(100, '\n');    size++;}
+	in.seekg(0);     return size;}
+	
+void readProcesses(ifstream &in, queue<Process> &Q, const int arrTime){
+	int size = sizeOfLines(in);
+	if (size > 0)
+	{int size1 = size + Q.size();    Process current_process;	bool flag = !Q.empty();
+		if (flag)
+		{ current_process = Q.front();	Q.pop();
+			size1--;	}
+		Process *list = new Process[size1];
+		for (int i = 0; i < size; i++){in >> list[i];   list[i].arrTime = arrTime;}
+		in.close();
+		ofstream out("readylist.txt", ios::trunc);
+		out.close();
+		in.open("readylist.txt");
+		for (int i = 0; size < size1; i++){list[size++] = Q.front();  Q.pop();}
+		cout << "The contents of ready queue are:" << endl;
+               
+		if (flag){Q.push(current_process);	cout << Q.front() << endl;}
+		for (int i = 0; i < size1; i++){Q.push(list[i]);   cout << list[i] << endl;}
+		delete []list;cout << endl << endl;
+	}
+	
+}
+void displayQueue(queue<Process> &m){
+	queue<Process> t = m;
+	cout << endl << "Processes in ready queue are:" << endl;
+	while (!t.empty())
+	{cout << t.front().name << "," << t.front().exeTime << " " << endl;  	t.pop();}
+}
+
---
+++ Calculator Language/Calculator Language.cpp
@@ -0,0 +1,241 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<cstring>
+#include<cmath>
+using namespace std;
+template <typename Object>
+class StackADT
+{
+public:
+	virtual bool isEmpty() const = 0;	virtual long sSize() const = 0;
+	virtual Object peek() const = 0;    virtual void push(const Object &) = 0;
+	virtual Object pop() = 0;
+};
+template <typename Object>
+class StackL : public StackADT<Object>
+{
+private:
+	class ListNode
+	{
+	public:
+		Object data;      ListNode* next;
+		ListNode(Object d)
+		{data = d;    next = NULL;	}
+	};
+	ListNode* head;long top;
+public:
+	StackL()
+	{head = NULL;	top = 0;}
+	~StackL()
+	{
+		while (head != NULL)
+		{ListNode *junk = head;    head = head->next;   delete junk;}
+		head = NULL;
+	}
+	bool isEmpty() const
+	{return (head == NULL);	}
+	long sSize() const
+	{return top;}
+	Object peek() const
+	{
+		if (isEmpty())
+		{return 'n';}
+		return head->data;
+	}
+	void push(const Object &d)
+	{
+		top++;
+		if (isEmpty())
+		{head = new ListNode(d);
+         	return;}
+		ListNode* tmp = new ListNode(d);
+		tmp->next = head;
+		head = tmp;
+	}
+	Object pop()
+	{
+		if (isEmpty())
+		{
+			throw 0;
+		}
+		ListNode* tmp = head;
+		head = head->next;
+		Object o = tmp->data;
+		delete tmp;
+		top--;
+		return o;
+	}
+};
+bool isOperator(char c)
+{
+	if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '(' || c == ')')
+		return true;
+	return false;
+}
+int preceDence(char op)
+{
+	if (op == '+' || op == '-')
+		return 1;
+	else if (op == '*' || op == '/')
+		return 2;
+	else if (op == '^')
+		return 3;
+	else
+		return -1;
+}
+string makeZero(string inFix) {
+	int i = inFix.length() - 1;
+	while (inFix[i] != '=' && i != 0) {
+		if (inFix[i] >= 'A' && inFix[i] <= 'Z') {
+			cout << inFix[i] << "=";
+			inFix[i] = '0';
+			cout << inFix[i] << ", ";
+		}
+		i--;
+	}
+	return inFix;
+}
+void infixToPostfix(const char* inFix, char* postFix)
+{
+	int k = 0;
+	StackADT<char> *s = new StackL<char>;
+	for (int i = 0; i<strlen(inFix); i++)
+	{
+		if (inFix[i] == ' ' || inFix[i] == '=')
+			continue;
+		else if(inFix[i] >= 'A' && inFix[i] <= 'Z')
+			postFix[k++] = inFix[i];
+		else if (!isOperator(inFix[i]) || inFix[i] == '~')
+		{
+			postFix[k++] = inFix[i];
+		}
+		else
+		{
+			if (preceDence(s->peek()) > preceDence(inFix[i]) && preceDence(inFix[i]) != -1)
+			{
+				postFix[k++] = s->pop();
+				s->push(inFix[i]);
+			}
+			else
+			{
+				if (inFix[i] == ')')
+				{
+					while (s->peek() != '(')
+					{
+						postFix[k++] = s->pop();
+					}
+					s->pop();
+				}
+				else
+				{
+					s->push(inFix[i]);
+				}
+			}
+		}
+	}
+	while (!s->isEmpty())
+	{
+		if (s->peek() != '(' || s->peek() != ')')
+			postFix[k++] = s->pop();
+		else
+			s->pop();
+	}
+	postFix[k] = '\0';
+}
+double opeRation(double op1, double op2, char opr)
+{
+	if (opr == '+')
+		return op1 + op2;
+	else if (opr == '-')
+		return op1 - op2;
+	else if (opr == '*')
+		return op1 * op2;
+	else if (opr == '/')
+	{
+		if (opr == 0)
+			throw 0;
+		return op1 / op2;
+	}
+	else if (opr == '^')
+		return pow(op1, op2);
+	else
+		throw 0;
+}
+double evaluatePostfix(const char* postFix)
+{
+	bool flag = false;
+	StackADT<int> *s = new StackL<int>;
+	string cr = "";
+	bool b = true;
+	for (int i = 0; i < strlen(postFix); i++)
+	{
+		if (postFix[i] >= 'A' && postFix[i] <= 'Z' && b) {
+			cr += postFix[i];
+			continue;
+		}
+		else if (postFix[i] >= 'A' && postFix[i] <= 'Z'){
+			continue;
+		}
+		else if (postFix[i] == '~') {
+			flag = true;
+		}
+		else if (!isOperator(postFix[i]))
+		{
+			if (flag) {
+				s->push(-1 * (int(postFix[i]) - 48));
+				flag = false;
+			}
+			else
+				s->push(int(postFix[i]) - 48);
+		}
+		else
+		{
+			int op2 = s->pop(), op1 = s->pop();
+			s->push(opeRation(op1, op2, postFix[i]));
+		}
+		b = false;
+	}
+	int i = 0;
+	while (i < cr.length()) {
+		cout <<cr[i] << "=";
+		if (s->peek() < 0)
+		{
+         		cout << '~';
+		        cout <<-s->peek();
+	     }
+		 else{
+		    	cout<<s->peek();
+		    	
+	   }
+	   
+		cout<<",";
+		i++;
+	}
+	
+	cout << endl;
+	return s->pop();
+}
+int main()
+{
+	ifstream inp("Input.txt");
+	if (inp.fail())
+	{
+		cout << "Error in loading file" << endl;
+	}
+	else{
+	string s;
+	while (inp.peek() != EOF) {
+		getline(inp, s);
+		if (s == "#") {
+			inp.close();
+			return 0;
+		}
+		s = makeZero(s);
+		char *s2 = new char[s.length()];
+		infixToPostfix(s.c_str(), s2);
+		evaluatePostfix(s2);
+		delete[] s2;
+	}
+    }
+}
